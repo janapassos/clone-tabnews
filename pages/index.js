5df7e757911c599f342b0b8cd4b0a120032e8dbf
janapassos <107141674+janapassos@users.noreply.github.com>
2023-10-25 13:46:30 +0000

    acerto

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function Home() {
-    return <h1>Rogério, Sobrou chocolate??? Se não comeu tudo, me dê um tikim, pufavôSkkk</h1>
+    return <h1>Rogério, Sobrou chocolate??? Se não comeu tudo, me dê um tikim, pufavôkkk</h1>
 }
 export default Home;
