658a3bbad30e6f9144ce6698a6c9a40ccf2053d0
janapassos <107141674+janapassos@users.noreply.github.com>
2025-07-30 01:14:54 +0000

    mensagem mae

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,8 @@
 function Home() {
-  return <h1>Fátima, Boa noite ou bom dia!!! Treinando programação kkkkkk</h1>;
+  return (
+    <h1>
+      Mãe, Boa noite ou bom dia!!! Treinando programação, tecnologia kkkkkk
+    </h1>
+  );
 }
 export default Home;
